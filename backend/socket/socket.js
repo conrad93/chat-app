ed75f271a2533a95aec5736e16abb0bf17ba7c0b
conrad93 <nunes.conrad93@gmail.com>
2024-05-13 17:48:53 +0530

    socket url

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:4200"],
+        origin: ["https://chat-app-o6ie.onrender.com"],
         method: ["GET", "POST"]
     }
 });
